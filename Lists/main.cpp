35fbef0c27069b9303521b4db742d4bad15c0af2
okprogrammmer <omk7912@gmail.com>
2018-04-28 15:54:22 +0530

    revesing,sorting

--- Lists/main.cpp
+++ Lists/main.cpp
@@ -9,32 +9,46 @@ int main()
     numbers.push_back(1);//pushing end of the list
     numbers.push_back(2);
     numbers.push_back(3);
-    numbers.push_front(0);
+    numbers.push_front(0);//pushing at start of list
 
-    list<int>::iterator it=numbers.begin();
+    list<int>::iterator it=numbers.begin();//assigning the begin address
     it++;
-    numbers.insert(it,100);
+    numbers.insert(it,100);//inserting 100 at second position
     cout<<"Element: "<<*it<<endl;
 
     list<int>::iterator eraseit=numbers.begin();
     eraseit++;
-    numbers.erase(eraseit);
+    numbers.erase(eraseit);//erasing second element
     cout<<"Element: "<<*eraseit<<endl;
 
     for(list<int>::iterator it=numbers.begin();it!=numbers.end();)
     {
        if(*it==2)
        {
-           numbers.insert(it,1234);
+           numbers.insert(it,1234);//inserting where element is 2
        }
        if(*it==1){
-        it=numbers.erase(it);
+        it=numbers.erase(it);//removing element 1
        }
        else{
         it++;
        }
     }
+    for(list<int>::iterator it=numbers.begin();it!=numbers.end();it++)//printing the all the element in the list
+    {
+        cout<<*it<<endl;
+    }
+    numbers.sort();
+    cout<<"list after sorting!\n"<<endl;
     for(list<int>::iterator it=numbers.begin();it!=numbers.end();it++)
+
+    {
+        cout<<*it<<endl;
+    }
+    numbers.reverse();
+    cout<<"list after reversing!"<<endl;
+    for(list<int>::iterator it=numbers.begin();it!=numbers.end();it++)
+
     {
         cout<<*it<<endl;
     }
